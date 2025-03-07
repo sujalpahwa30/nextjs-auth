de1c30a2fedac69ab841d8e63688a90d4249199f
Sujal Pahwa <sujalpahwa4@gmail.com>
2025-03-07 18:16:32 +0530

    feat: setup the login, signup page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function ProfilePage() {
+    return (
+        <div className="flex">
+            <h1 className="text-center text-white text-5xl">Profile Page</h1>
+        </div>
+    )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
+export default function SignupPage() {
+    return (
+        <div className="flex">
+            <h1 className="text-center text-white text-5xl">Signup Page</h1>
+        </div>
+    )
+}
